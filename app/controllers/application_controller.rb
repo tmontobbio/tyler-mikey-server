e0d93e56d16e652f35ce8e86e7998298b51efcb0
Tyler <toygrrr@gmail.com>
2022-09-13 16:15:09 -0700

    seeded and set up requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,42 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  # Read
+  get '/records' do
+    records = Record.all
+    records.to_json
+  end
+
+  # getting with perams params[:id]
+  get "/records/:id" do
+    record = Record.find(params[:id])
+    record.to_json
+  end
+  #Create
+  post '/records' do
+    record = Record.create(record_params)
+    status 201
+    record.to_json
+  end
+
+  #Update
+  patch '/records/:id' do
+    record = Record.find(params[:id])
+    record.update(record_params)
+    record.to_json
+    status 200
+  end
+
+  #Destroy
+  delete '/records/:id' do
+    record = Recorc.find(params[:id])
+    record.Destroystatus 204
+  end
+
+  private
+
+  def record_params
+    {artist:params[:artist], album:params[:album], year:params[:year], image_url:params[:image_url]}
   end
 
 end
